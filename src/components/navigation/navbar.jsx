4964b6f583054b09b9362daee72a723a3c03c966
oscarrryeezus <0scardanielmn20040428@gmail.com>
2024-10-06 12:08:59 -0600

    Le movi al navbar

--- src/components/navigation/navbar.jsx
+++ src/components/navigation/navbar.jsx
@@ -10,29 +10,27 @@ import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import ListItemText from '@mui/material/ListItemText';
-import MailIcon from '@mui/icons-material/Mail';
-import MenuIcon from '@mui/icons-material/Menu';
+import Avatar from '@mui/material/Avatar';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
-import Avatar from '@mui/material/Avatar';
-import Stack from '@mui/material/Stack';
-import HistoryIcon from '@mui/icons-material/History';
-import PaymentIcon from '@mui/icons-material/Payment';
-import MopedIcon from '@mui/icons-material/Moped';
 import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
-import LogoutIcon from '@mui/icons-material/Logout';
+import MenuIcon from '@mui/icons-material/Menu';
 import { useState } from 'react';
-import { Home } from '@mui/icons-material';
+import { Home, Logout, Payment, History, Moped } from '@mui/icons-material';
+
 const drawerWidth = 240;
 
 export default function Navbar() {
   const [mobileOpen, setMobileOpen] = useState(false);
-  const [isClosing, setIsClosing] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen(!mobileOpen);
+  };
+
   function stringToColor(string) {
     let hash = 0;
     let i;
   
-    /* eslint-disable no-bitwise */
     for (i = 0; i < string.length; i += 1) {
       hash = string.charCodeAt(i) + ((hash << 5) - hash);
     }
@@ -43,11 +41,10 @@ export default function Navbar() {
       const value = (hash >> (i * 8)) & 0xff;
       color += `00${value.toString(16)}`.slice(-2);
     }
-    /* eslint-enable no-bitwise */
   
     return color;
   }
-  
+
   function stringAvatar(name) {
     return {
       sx: {
@@ -57,36 +54,13 @@ export default function Navbar() {
     };
   }
 
-  const handleDrawerClose = () => {
-    setIsClosing(true);
-    setMobileOpen(false);
-  };
-
-  const handleDrawerTransitionEnd = () => {
-    setIsClosing(false);
-  };
-
-  const handleDrawerToggle = () => {
-    setMobileOpen(!mobileOpen);
-  };
-
-  
-  const routes = [
-    { route: 'allPages/', label: 'Home', icon: <Home /> },
-    { route: 'allPages/carrito', label: 'Mi Carrito', icon: <ShoppingCartIcon /> },
-    { route: 'allPages/pedido', label: 'Mi Pedido', icon: <MopedIcon /> },
-    { route: 'allPages/historal', label: 'Historial de compras', icon: <HistoryIcon /> },
-    { route: 'allPages/payment', label: 'Formas de Pago', icon: <PaymentIcon /> },
-    { route: '/', label: 'Salir', icon: <LogoutIcon /> },
-  ];
-  
   const drawer = (
     <div>
-      <List >
+      <List>
         <ListItem disablePadding>
-          <ListItemButton sx={{height:47}} href='/allPages/profile'>
+          <ListItemButton sx={{ height: 47 }} href="/allPages/profile">
             <ListItemIcon>
-              <Avatar  {...stringAvatar('Adrian Morales')} />
+              <Avatar {...stringAvatar('Adrian Morales')} />
             </ListItemIcon>
             <ListItemText primary="Hola Adrian" />
           </ListItemButton>
@@ -94,13 +68,18 @@ export default function Navbar() {
       </List>
       <Divider />
       <List>
-        {routes.map((route, index) => (
-          <ListItem key={route.route} disablePadding>
-            <ListItemButton href={`/${route.route}`}>
+        {[{ label: 'Home', icon: <Home />, route: 'allPages/' },
+          { label: 'Mi Carrito', icon: <ShoppingCartIcon />, route: 'allPages/carrito' },
+          { label: 'Mi Pedido', icon: <Moped />, route: 'allPages/pedido' },
+          { label: 'Historial de compras', icon: <History />, route: 'allPages/historial' },
+          { label: 'Formas de Pago', icon: <Payment />, route: 'allPages/payment' },
+          { label: 'Salir', icon: <Logout />, route: '/' }].map((item) => (
+          <ListItem key={item.route} disablePadding>
+            <ListItemButton href={`/${item.route}`}>
               <ListItemIcon>
-                {route.icon}
+                {item.icon}
               </ListItemIcon>
-              <ListItemText primary={route.label} />
+              <ListItemText primary={item.label} />
             </ListItemButton>
           </ListItem>
         ))}
@@ -112,42 +91,43 @@ export default function Navbar() {
     <Box sx={{ display: 'flex' }}>
       <CssBaseline />
       <AppBar
-      position="fixed"
-      sx={{
-        bgcolor: '#077d6b',
-        width: { sm: `calc(100% - ${drawerWidth}px)` },
-        ml: { sm: `${drawerWidth}px` },
-      }}
-    >
-      <Toolbar>
-        <IconButton
-          color="inherit"
-          aria-label="open drawer"
-          edge="start"
-          onClick={handleDrawerToggle}
-          sx={{ mr: 2, display: { sm: 'none' } }}
-        >
-          <MenuIcon />
-        </IconButton>
-        <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
-          Sr. Macondo App
-        </Typography>
-        
-        {/* Icono de carrito de compras */}
-        <IconButton color="inherit" aria-label="shopping cart" sx={{ ml: 2 }} href='/allPages/carrito'>
-          <ShoppingCartIcon />
-        </IconButton>
-      </Toolbar>
-    </AppBar>
-      <Box component="nav" sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }} aria-label="mailbox folders">
+        position="fixed"
+        sx={{
+          bgcolor: '#077d6b',
+          width: { sm: `calc(100% - ${drawerWidth}px)` },
+          ml: { sm: `${drawerWidth}px` },
+        }}
+      >
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: 'none' } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap component="div" sx={{ flexGrow: 1 }}>
+            Sr. Macondo App
+          </Typography>
+          <IconButton color="inherit" href="/allPages/carrito">
+            <ShoppingCartIcon />
+          </IconButton>
+        </Toolbar>
+      </AppBar>
+
+      <Box
+        component="nav"
+        sx={{ width: { sm: drawerWidth }, flexShrink: { sm: 0 } }}
+        aria-label="mailbox folders"
+      >
+        {/* Drawer para móviles */}
         <Drawer
           variant="temporary"
           open={mobileOpen}
-          onTransitionEnd={handleDrawerTransitionEnd}
-          onClose={handleDrawerClose}
-          ModalProps={{
-            keepMounted: true, // Better open performance on mobile.
-          }}
+          onClose={handleDrawerToggle}
+          ModalProps={{ keepMounted: true }}
           sx={{
             display: { xs: 'block', sm: 'none' },
             '& .MuiDrawer-paper': { boxSizing: 'border-box', width: drawerWidth },
@@ -155,6 +135,8 @@ export default function Navbar() {
         >
           {drawer}
         </Drawer>
+
+        {/* Drawer permanente para pantallas grandes */}
         <Drawer
           variant="permanent"
           sx={{
